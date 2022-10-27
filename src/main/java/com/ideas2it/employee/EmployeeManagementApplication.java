dc1578bbe541ebff0ca29eafe8c76257992a249d
Rasika-Venkadesh <rasika.venkadesh@ideas2it.com>
2022-10-27 20:31:50 +0530

    initial SpringSecurity commit

--- src/main/java/com/ideas2it/employee/EmployeeManagementApplication.java
+++ src/main/java/com/ideas2it/employee/EmployeeManagementApplication.java
@@ -1,19 +1,18 @@
 package com.ideas2it.employee;
 
+import com.ideas2it.employee.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class EmployeeManagementApplication {
-
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
